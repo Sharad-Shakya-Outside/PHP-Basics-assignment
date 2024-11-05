a111c06f4fe0deb2c9d90fb571759585bf815589
SharadShakya9 <shakyasharad9@gmail.com>
2024-11-05 14:40:18 +0545

    container made flex - day 3

--- day3/signup.php
+++ day3/signup.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP basic Day 3 - Login and Signup</title>
+    <link rel="stylesheet" href="signup.css">
 </head>
 
 <body>
@@ -15,8 +16,23 @@
 
         <section class="right-side">
             <form action="login.php" method="post">
-                
+                <div class="form-element">
+                    <label for="username">Username</label>
+                    <input type="text" id="username">
+                </div>
+                <div class="form-element">
+                    <label for="password">Password</label>
+                    <input type="password" id="password">
+                </div>
+                <div class="form-element">
+                    <label for="confirm">Confirm password</label>
+                    <input type="password" id="password">
+                </div>
+
+                <button type="submit">Sign Up</button>
             </form>
+            
+            <p>Already signed up? <a href="login.php">Sign In</a></p>
         </section>
     </main>
 </body>
